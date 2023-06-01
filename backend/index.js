e87bfcebf5a171bf64d710cff21c3ae3e01b5052
jrabadines <100257552+jhiedy@users.noreply.github.com>
2023-06-01 16:20:23 +0800

    refactor routes

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import cloudinary from 'cloudinary';
+import cors from 'cors';
 
 // import UserSchema from "./models/user.js";
 import "./controller.js";
 import setUpRoutes from "./routes.js";
-import { dbConfig } from "./config/db.config.js";
+// import { dbConfig } from "./config/db.config.js";
 
 // connect to Mongo DB
 // mongoose.connect(`
@@ -24,6 +25,7 @@ cloudinary.config({
   api_key: "166751186939533",
   api_secret: "meKTGcLeSjyv9V1B0-gqGcHfNmI"
 });
+
 
 try {
   mongoose.connect("mongodb+srv://128team:3x_3d_3xWjdcVhp@128-stals.nl5qcfg.mongodb.net/?retryWrites=true&w=majority", {
@@ -45,10 +47,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+// app.use(cors());
 
 // allow CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000", "https://elbi-space.onrender.com");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Methods, Origin, Accept, Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", "true");
